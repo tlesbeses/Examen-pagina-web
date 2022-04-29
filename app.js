85b598205984ceada39a0ce8cbe44510e68460b8
kevin cardoza <pevin.kevin@gmail.com>
2022-04-29 17:05:49 -0600

    comit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function calcular(){
     var Salario = document.getElementById("salario").value
-
+    if (Salario>0) {
     var SalarioB = document.getElementById("resultado_bruto")
     SalarioB.innerHTML = Salario
 
@@ -16,30 +16,36 @@ function calcular(){
     var InpuestoSR = document.getElementById("resultado_IR")
     var salarioTCD = document.getElementById("resultado_deduccioens")
     var calculo
-    if (salarioAnual<=100000) {
-        calculo = 0
-        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
-    }  else if(salarioAnual>100000 && salarioAnual<=200000 ) {
-        calculo = ((salarioAnual-100000)*0.15)/12
-        InpuestoSR.innerHTML = calculo.toFixed(2);
-        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+    
+        if (salarioAnual<=100000) {
+            calculo = 0
+            InpuestoSR.innerHTML = 0
+            salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
 
-    }else if(salarioAnual>200000 && salarioAnual<=350000){
-        calculo = (((salarioAnual-200000)*0.2)+15000)/12
-        InpuestoSR.innerHTML = calculo.toFixed(2);
-        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
-
-    }else if (salarioAnual>350000 && salarioAnual<=500000) {
-        calculo = (((salarioAnual-350000)*0.25)+45000)/12
-        InpuestoSR.innerHTML = calculo.toFixed(2);
-        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
-
-    } else if (salarioAnual>500000) {
-        calculo = (((salarioAnual-500000)*0.3)+82500)/12
-        InpuestoSR.innerHTML = calculo.toFixed(2);
-        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
-    } else {
-        
+        }  else if(salarioAnual>100000 && salarioAnual<=200000 ) {
+            calculo = ((salarioAnual-100000)*0.15)/12
+            InpuestoSR.innerHTML = calculo.toFixed(2);
+            salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+    
+        }else if(salarioAnual>200000 && salarioAnual<=350000){
+            calculo = (((salarioAnual-200000)*0.2)+15000)/12
+            InpuestoSR.innerHTML = calculo.toFixed(2);
+            salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+    
+        }else if (salarioAnual>350000 && salarioAnual<=500000) {
+            calculo = (((salarioAnual-350000)*0.25)+45000)/12
+            InpuestoSR.innerHTML = calculo.toFixed(2);
+            salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+    
+        } else if (salarioAnual>500000) {
+            calculo = (((salarioAnual-500000)*0.3)+82500)/12
+            InpuestoSR.innerHTML = calculo.toFixed(2);
+            salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+        } else {
+            
+        }
+    }else{
+        alert("no peudes poner numeros negativos o ceros")
     }
 
 

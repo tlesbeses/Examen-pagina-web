673e5dacf16fef81f4321e459a3adb838666a211
kevin cardoza <pevin.kevin@gmail.com>
2022-04-29 16:57:00 -0600

    commit

--- app.js
+++ app.js
@@ -18,13 +18,28 @@ function calcular(){
     var calculo
     if (salarioAnual<=100000) {
         calculo = 0
-        salarioTCD.innerHTML = SalarioN-calculo
-    }  else if(salarioAnual>100000 || salarioAnual>200000 ) {
+        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+    }  else if(salarioAnual>100000 && salarioAnual<=200000 ) {
         calculo = ((salarioAnual-100000)*0.15)/12
-        InpuestoSR.innerHTML = calculo;
-        salarioTCD.innerHTML = SalarioN-calculo
-    }else{
+        InpuestoSR.innerHTML = calculo.toFixed(2);
+        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
 
+    }else if(salarioAnual>200000 && salarioAnual<=350000){
+        calculo = (((salarioAnual-200000)*0.2)+15000)/12
+        InpuestoSR.innerHTML = calculo.toFixed(2);
+        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+
+    }else if (salarioAnual>350000 && salarioAnual<=500000) {
+        calculo = (((salarioAnual-350000)*0.25)+45000)/12
+        InpuestoSR.innerHTML = calculo.toFixed(2);
+        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+
+    } else if (salarioAnual>500000) {
+        calculo = (((salarioAnual-500000)*0.3)+82500)/12
+        InpuestoSR.innerHTML = calculo.toFixed(2);
+        salarioTCD.innerHTML = SalarioN-calculo.toFixed(2)
+    } else {
+        
     }
 
 
